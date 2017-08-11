5eb242ea8c0e87ab4e4ee3957a13741ef2168b3b
KyuHyuk Lee <lee@kyuhyuk.kr>
2017-08-12 03:23:57 +0900

    Update: 'check_source' function. - Add '--content-disposition' option.

--- support/functions.mk
+++ support/functions.mk
@@ -1,16 +1,17 @@
 define check_source
 	@if ! [ -f $(1) ] ; then \
-		wget $(3) -c -P $(SOURCES_DIR) 2>&1 >/dev/null; true ; \
+		wget $(3) -c -P $(SOURCES_DIR) --content-disposition 2>&1 >/dev/null; true ; \
 	else \
 		if ! [ `md5sum $(1)  | cut -d ' ' -f 1` = $(2) ] ; then \
-			wget $(3) -c -P $(SOURCES_DIR) 2>&1 >/dev/null; true ; \
+			wget $(3) -c -P $(SOURCES_DIR) --content-disposition 2>&1 >/dev/null; true ; \
 		fi; \
 	fi;
 endef
 
 define dependency_libs_patch
 	for lib in $(SYSROOT_DIR)/usr/lib/*.la ; do \
-		dependency_libs_line=`grep -n "dependency_libs=" $${lib} | cut -d':' -f1` ; \
-		sed -i "s@ /usr/lib/\(.*\).la@ $(SYSROOT_DIR)/usr/lib/\1.la@g" $${lib} ; \
+		while ! [ `grep " /usr/lib/\(.*\).la" $${lib}` -n ] ; do \
+			sed -i "s@ /usr/lib/\(.*\).la@ $(SYSROOT_DIR)/usr/lib/\1.la@g" $${lib} ; \
+		done ; \
 	done;
 endef
